7d3da78ded538a758762f0acef608fdf029ce895
David Hermann <redeagle.private@gmail.com>
2023-01-04 02:39:30 +0100

    Added JSONParser.java to handle read and search for key events.

--- src/main/java/org/bitbiome/Main.java
+++ src/main/java/org/bitbiome/Main.java
@@ -1,10 +1,15 @@
 package org.bitbiome;
 
+import org.json.JSONObject;
+
+import java.util.Scanner;
+
 public class Main {
 
+    private Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JSONObject playerConfig = JsonParser.getJSONObject("playerconfig.json");
     }
     public static int getLucky() {
         return 7;
