b3dfbe360189c4ced486e25ebbfe3c7a0f4f52a9
David Hermann <redeagle.private@gmail.com>
2023-01-05 21:20:24 +0100

    refactor: outsourced code in String function

--- src/main/java/org/bitbiome/commands/QuitCommand.java
+++ src/main/java/org/bitbiome/commands/QuitCommand.java
@@ -6,7 +6,11 @@ public class QuitCommand implements CommandAPI {
 
     @Override
     public void performCommand(Scanner scanner, boolean isRunning, String message) {
-        System.out.println("You quitted!");
+        System.out.println(getQuitMessage());
         System.exit(0);
+    }
+
+    public static String getQuitMessage() {
+        return "You quitted!";
     }
 }
