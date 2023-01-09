cfa4e9de4e5d62902b539f99feb677142a60fe26
Max Gerbeth <max.gerbeth03@gmail.com>
2023-01-09 10:29:24 +0100

    Added ShopCommand.java

---
+++ src/main/java/org/bitbiome/commands/ShopCommand.java
@@ -0,0 +1,11 @@
+package org.bitbiome.commands;
+
+import java.util.Scanner;
+
+public class ShopCommand implements CommandAPI{
+
+    @Override
+    public void performCommand(Scanner scanner, boolean isRunning, String message) {
+
+    }
+}
