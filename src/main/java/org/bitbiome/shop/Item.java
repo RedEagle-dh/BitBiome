31aea13df574e525fb44d7732348e1b0f7adbebe
Max Gerbeth <max.gerbeth03@gmail.com>
2023-01-09 16:50:23 +0100

    Constructor in Item.java

--- src/main/java/org/bitbiome/shop/Item.java
+++ src/main/java/org/bitbiome/shop/Item.java
@@ -1,7 +1,16 @@
 package org.bitbiome.shop;
 
 public class Item {
-    public Item(){
+    public String name;
+    public String damage;
+    public boolean crafting;
+    public int durability;
 
+    public Item(String name, String damage, boolean crafting, int durability){
+        this.name = name;
+        this.damage = damage;
+        this.crafting = crafting;
+        this.durability = durability;
     }
+
 }
