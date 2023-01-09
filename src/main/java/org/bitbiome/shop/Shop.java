19f227ebe83ac4775133550f7b07ae99155fa95e
Max Gerbeth <max.gerbeth03@gmail.com>
2023-01-09 17:01:10 +0100

    Method headers added to Items.java

--- src/main/java/org/bitbiome/shop/Shop.java
+++ src/main/java/org/bitbiome/shop/Shop.java
@@ -1,7 +1,35 @@
 package org.bitbiome.shop;
 
+import java.util.ArrayList;
+
 public class Shop {
     public Shop(){
 
+    }
+
+    public boolean kaufen(){
+        //ToDo
+
+        return true;
+    }
+
+    private ArrayList itemsLaden(){
+        //ToDo
+
+        return null;
+    }
+
+    public ArrayList<Item> itemRotation(ArrayList<Item> alleItems, int anzahlItemsImShop){
+        //ToDo
+
+        return null;
+    }
+
+    public ArrayList<Item> aktuellImShop(){
+        return null;
+    }
+
+    public void Quiz(){
+        //ToDo
     }
 }
