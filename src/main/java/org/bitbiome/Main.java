13f6640dc7cecd4a846bacf0bbeb807538f297e9
David Hermann <redeagle.private@gmail.com>
2023-01-11 18:39:19 +0100

    refactoring: Moved InteractionLoop and JsonParser to the new directory "classes". Removed test code from JsonParser. Removed comment in Main

--- src/main/java/org/bitbiome/Main.java
+++ src/main/java/org/bitbiome/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //JSONObject playerConfig = JsonParser.getJSONObject("playerconfig.json");
         new Boot();
     }
 
