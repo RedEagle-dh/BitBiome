ebfcbc1e7e94c84e61db465685b032c1031ed660
fdai7430 <philipp.voeller@informatik.hs-fulda.de>
2023-01-15 20:56:37 +0100

    Added class for use command

---
+++ src/main/java/org/bitbiome/commands/UseCommand.java
@@ -0,0 +1,9 @@
+package org.bitbiome.commands;
+
+import java.util.Scanner;
+
+public class UseCommand implements CommandAPI {
+    @Override
+    public void performCommand(Scanner scanner, boolean isRunning, String message) {
+    }
+}
