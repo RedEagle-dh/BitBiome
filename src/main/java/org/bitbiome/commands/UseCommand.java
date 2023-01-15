80197d15f5a767431107783d050c1bff1aea161f
fdai7430 <philipp.voeller@informatik.hs-fulda.de>
2023-01-15 21:21:31 +0100

    Added function for executing use command

--- src/main/java/org/bitbiome/commands/UseCommand.java
+++ src/main/java/org/bitbiome/commands/UseCommand.java
@@ -3,7 +3,12 @@ package org.bitbiome.commands;
 import java.util.Scanner;
 
 public class UseCommand implements CommandAPI {
+    private String getUseMessage(String msg) {
+        return "";
+    }
+
     @Override
     public void performCommand(Scanner scanner, boolean isRunning, String message) {
+        System.out.println(getUseMessage(message.split(" ", 2)[1]));
     }
 }
