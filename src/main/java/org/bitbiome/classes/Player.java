6e24f5651ba4776c5cea8d5870d7de4a6fe17bdc
David Hermann <redeagle.private@gmail.com>
2023-01-18 18:25:14 +0100

    Created player class

---
+++ src/main/java/org/bitbiome/classes/Player.java
@@ -0,0 +1,51 @@
+package org.bitbiome.classes;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private float hp;
+    private Location location;
+
+    private ArrayList<Item> inventory;
+
+    public Player(String name) {
+        this.name = name;
+        hp = 100.0F;
+        location = CreateLocations.createForest();
+        inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    public float getHp() {
+        return hp;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setHp(float hp) {
+        this.hp = hp;
+    }
+
+    public boolean addToInventory(Item item) {
+        return inventory.add(item);
+    }
+
+    public boolean removeFromInventory(Item item) {
+        return inventory.remove(item);
+    }
+
+}
