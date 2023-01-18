20bc727d77f2cb6dd1d01af37cb3b25c7f2ba8be
David Hermann <redeagle.private@gmail.com>
2023-01-18 18:25:31 +0100

    Created traveling engine

---
+++ src/main/java/org/bitbiome/classes/TravelEngine.java
@@ -0,0 +1,15 @@
+package org.bitbiome.classes;
+
+public class TravelEngine {
+
+    private Location location;
+
+    public TravelEngine() {
+
+    }
+
+    public void travelTo(Location location) {
+        this.location = location;
+    }
+
+}
