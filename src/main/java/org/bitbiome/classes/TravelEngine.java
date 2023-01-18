157ceabc59f7473671646ce2d213005746065c68
David Hermann <redeagle.private@gmail.com>
2023-01-18 19:32:30 +0100

    Edited TravelEngine, added getLocationList and locationExist method

--- src/main/java/org/bitbiome/classes/TravelEngine.java
+++ src/main/java/org/bitbiome/classes/TravelEngine.java
@@ -2,13 +2,19 @@ package org.bitbiome.classes;
 
 import org.bitbiome.entities.Location;
 import org.bitbiome.entities.Player;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 public class TravelEngine {
 
-    private Location location;
+    private JSONArray locations;
+    JsonParser jp;
     private Player player;
 
     public TravelEngine(Player player) {
+        jp = new JsonParser();
+        locations = jp.getJSONObject("../gameconfig.json").getJSONArray("locations");
         this.player = player;
     }
 
@@ -18,6 +24,19 @@ public class TravelEngine {
 
     public Player getPlayer() {
         return player;
+    }
+
+    public JSONArray getLocationList() {
+        return locations;
+    }
+
+    public boolean locationExists(String name) {
+        boolean found = false;
+        for (int i = 0; i < locations.length(); i++)
+            if (locations.getJSONObject(i).getString("name").equals(name)) {
+                found = true;
+            }
+        return found;
     }
 
 }
