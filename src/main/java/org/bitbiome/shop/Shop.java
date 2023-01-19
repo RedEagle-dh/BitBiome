ec8fd8524be0d7d1e699b1f9986b628b1f1dfd16
Max Gerbeth <max.gerbeth03@gmail.com>
2023-01-20 00:14:36 +0100

    remove unused import in Shop.java

--- src/main/java/org/bitbiome/shop/Shop.java
+++ src/main/java/org/bitbiome/shop/Shop.java
@@ -5,7 +5,6 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
