0ecbfdc69dbae40138ae68ead1d43635d5717c69
Max Gerbeth <max.gerbeth03@gmail.com>
2023-01-20 00:02:23 +0100

    unittest: testSubtractGold3()

--- src/test/java/org/bitbiome/commands/ShopCommandTest.java
+++ src/test/java/org/bitbiome/commands/ShopCommandTest.java
@@ -53,4 +53,10 @@ public class ShopCommandTest {
         int result = shop.subtractGold(7, 3);
         assertEquals(expected, result);
     }
+    @Test
+    public void testSubtractGold3(){
+        int expected = 5;
+        int result = shop.subtractGold(10, 5);
+        assertEquals(expected, result);
+    }
 }
