ac443832b7913f72d76d217908511f13c22cb9eb
Max Gerbeth <max.gerbeth03@gmail.com>
2023-01-19 23:42:48 +0100

    unittest: testValidInput2

--- src/test/java/org/bitbiome/commands/ShopCommandTest.java
+++ src/test/java/org/bitbiome/commands/ShopCommandTest.java
@@ -8,9 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ShopCommandTest {
     final ShopCommand shopCommand = new ShopCommand();
     @Test
-    public void testValidInput(){
+    public void testValidInput1(){
         boolean expected = true;
         boolean result = shopCommand.validInput("1");
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testValidInput2(){
+        boolean expected = true;
+        boolean result = shopCommand.validInput("2");
         assertEquals(expected, result);
     }
 }
