0936d9f39c477759b8f528f08d7c8e0b9dc6fadb
Juliakn66 <117075450+Juliakn66@users.noreply.github.com>
2023-01-27 11:45:48 +0100

    implementing readInv()

--- src/main/java/org/bitbiome/commands/InventoryCommand.java
+++ src/main/java/org/bitbiome/commands/InventoryCommand.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class InventoryCommand implements CommandAPI{
     @Override
     public void performCommand(Scanner scanner, boolean isRunning, String message, TravelEngine travelEngine) {
-        
+        System.out.println(readInv(travelEngine));
     }
     public String readInv(TravelEngine travelEngine){
         return "";
