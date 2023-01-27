56792352b6fff2d62d45468343586882a363aa54
Juliakn66 <117075450+Juliakn66@users.noreply.github.com>
2023-01-27 14:00:02 +0100

    create lookaroundCommand.java

---
+++ src/main/java/org/bitbiome/commands/LookaroundCommand.java
@@ -0,0 +1,12 @@
+package org.bitbiome.commands;
+
+import org.bitbiome.classes.TravelEngine;
+
+import java.util.Scanner;
+
+public class LookaroundCommand implements CommandAPI{
+    @Override
+    public void performCommand(Scanner scanner, boolean isRunning, String message, TravelEngine travelEngine) {
+        
+    }
+}
