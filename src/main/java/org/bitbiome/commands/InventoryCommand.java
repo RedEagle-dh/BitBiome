3a54eff3082b414e22b2e41db49d8ec43f8d9c1b
Juliakn66 <117075450+Juliakn66@users.noreply.github.com>
2023-01-27 12:13:33 +0100

    edited method readInv()
    puts inventory out

--- src/main/java/org/bitbiome/commands/InventoryCommand.java
+++ src/main/java/org/bitbiome/commands/InventoryCommand.java
@@ -1,15 +1,31 @@
 package org.bitbiome.commands;
 
+import org.bitbiome.classes.JsonParser;
 import org.bitbiome.classes.TravelEngine;
+import org.bitbiome.entities.Item;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class InventoryCommand implements CommandAPI{
+public class InventoryCommand implements CommandAPI {
     @Override
     public void performCommand(Scanner scanner, boolean isRunning, String message, TravelEngine travelEngine) {
+
         System.out.println(readInv(travelEngine));
     }
-    public String readInv(TravelEngine travelEngine){
-        return "";
+
+    public String readInv(TravelEngine travelEngine) {
+        StringBuilder s = new StringBuilder();
+        JsonParser jp = new JsonParser();
+        JSONObject o = jp.getJSONObject("playerconfig.json");
+
+        ArrayList<Item> inventory = travelEngine.getPlayer().getInventory();
+        s.append("Inventory: \n");
+        for (int i=0; i < inventory.size(); i++){
+            s.append("- ").append(inventory.get(i).getName()).append("\n");
+        }
+
+        return s.toString();
     }
 }
