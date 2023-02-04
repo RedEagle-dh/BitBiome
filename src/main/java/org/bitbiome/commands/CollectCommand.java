841665d081d6617f35582a947c5e32534e5e856e
Juliakn66 <117075450+Juliakn66@users.noreply.github.com>
2023-02-04 10:36:32 +0100

    created method collectItem() to collectCommand

--- src/main/java/org/bitbiome/commands/CollectCommand.java
+++ src/main/java/org/bitbiome/commands/CollectCommand.java
@@ -1,12 +1,22 @@
 package org.bitbiome.commands;
 
+import org.bitbiome.classes.JsonParser;
 import org.bitbiome.classes.TravelEngine;
+import org.bitbiome.entities.Item;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CollectCommand implements CommandAPI {
     @Override
     public void performCommand(Scanner scanner, boolean isRunning, String message, TravelEngine travelEngine) {
-        
+
+    }
+
+    public void collectItem(ArrayList<Item> location, String item, JsonParser jp, JSONObject o, JSONArray inventory, TravelEngine travelEngine) {
+       
     }
 }
+
