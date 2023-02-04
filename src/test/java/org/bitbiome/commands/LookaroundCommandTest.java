42e710c76d600facba22b8d7653672c4f6d51205
Juliakn66 <117075450+Juliakn66@users.noreply.github.com>
2023-02-04 14:09:26 +0100

    unittest: added testGetRandomMob()

--- src/test/java/org/bitbiome/commands/LookaroundCommandTest.java
+++ src/test/java/org/bitbiome/commands/LookaroundCommandTest.java
@@ -1,11 +1,16 @@
 package org.bitbiome.commands;
 
+import org.bitbiome.entities.Item;
+import org.bitbiome.entities.Mob;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class LookaroundCommandTest {
@@ -35,6 +40,33 @@ public class LookaroundCommandTest {
         result = command.getLocationObject("not existing location", locations);
         assertEquals("Wald", result.getString("name"));
         assertEquals("Es gibt Bäume und Sträucher", result.getString("description"));
+    }
+
+    @Test
+    public void testGetRandomMob() {
+        LookaroundCommand command = new LookaroundCommand();
+        int randomNumberMobs = 2;
+        Random random = new Random();
+        JSONArray mobs = new JSONArray();
+        JSONObject mob1 = new JSONObject();
+        mob1.put("name", "BigFoot");
+        mob1.put("isFriendly", true);
+        mob1.put("hp", 10);
+        mob1.put("damage", 5);
+        JSONObject mob2 = new JSONObject();
+        mob2.put("name", "Yeti");
+        mob2.put("isFriendly", false);
+        mob2.put("hp", 20);
+        mob2.put("damage", 10);
+        mobs.put(mob1);
+        mobs.put(mob2);
+        ArrayList<Mob> foundMobs = new ArrayList<>();
+        ArrayList<Mob> result = command.getRandomMob(randomNumberMobs, random, mobs, foundMobs);
+
+        assertEquals(randomNumberMobs, result.size());
+        for (Mob mob : result) {
+            assertTrue(mob.getName().equals("BigFoot") || mob.getName().equals("Yeti"));
+        }
     }
 
 }
