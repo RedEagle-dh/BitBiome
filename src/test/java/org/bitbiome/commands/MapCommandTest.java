ea386d74617422840b1d353d8ce6e9cb6b5fa798
Tanja Herche <tanja.herche@informatik.hs-fulda.de>
2023-02-05 16:27:01 +0100

    Unittest: colored own location Gruenland

--- src/test/java/org/bitbiome/commands/MapCommandTest.java
+++ src/test/java/org/bitbiome/commands/MapCommandTest.java
@@ -130,7 +130,7 @@ public class MapCommandTest {
                 .append(" __________________________________________________________________________________________________\n")
                 .append("|                                |                                |                                |\n")
                 .append("|                                |                                |                                |\n")
-                .append("|             " + Colors.ANSI_BLUE + "Wueste" + Colors.ANSI_RESET + "             |           " + Colors.ANSI_BRIGHT_BLACK + "Gruenland" + Colors.ANSI_RESET + "            |           " + Colors.ANSI_BLUE + "Winterland" + Colors.ANSI_RESET + "           |" + "\n")
+                .append("|             " + Colors.ANSI_BLUE + "Wueste" + Colors.ANSI_RESET + "             |           " + Colors.ANSI_GREEN + "Gruenland" + Colors.ANSI_RESET + "            |           " + Colors.ANSI_BLUE + "Winterland" + Colors.ANSI_RESET + "           |" + "\n")
                 .append("|                                |                                |                                |\n")
                 .append("|________________________________|________________________________|________________________________|\n")
                 .append("|                                |                                |                                |\n")
