ada95c7e03706602cc70455e3300633b29dfa7bf
Friederike von Gruben <friederike.von-gruben@informatik.hs-fulda.de>
2023-02-06 13:34:20 +0100

    unittest: Created test for QuizCommand This test tests the start/welcome message to the quiz

---
+++ src/test/java/org/bitbiome/commands/QuizCommandTest.java
@@ -0,0 +1,13 @@
+package org.bitbiome.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QuizCommandTest {
+
+    @Test
+    public void testStartMessage() {
+        assertEquals("Du hast das Quiz gestartet! Hinweis: Wähle deine Antwort, indem du die Zahl (1-4) eingibst. Ist deine Lösung richtig, erhälst du 5 Münzen. Viel Erfolg! \n", QuizCommand.starterMessage());
+    }
+}
