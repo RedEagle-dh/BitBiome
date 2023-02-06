c8c68946b0d77614fea7e4e48cb2ded620e551a3
Friederike von Gruben <friederike.von-gruben@informatik.hs-fulda.de>
2023-02-06 13:35:52 +0100

    unittest: Edited test for QuizCommand This test tests the ending message for the quiz

--- src/test/java/org/bitbiome/commands/QuizCommandTest.java
+++ src/test/java/org/bitbiome/commands/QuizCommandTest.java
@@ -10,4 +10,9 @@ public class QuizCommandTest {
     public void testStartMessage() {
         assertEquals("Du hast das Quiz gestartet! Hinweis: Wähle deine Antwort, indem du die Zahl (1-4) eingibst. Ist deine Lösung richtig, erhälst du 5 Münzen. Viel Erfolg! \n", QuizCommand.starterMessage());
     }
+
+    @Test
+    public void testEndMessage() {
+        assertEquals("Das Quiz ist vorbei!", QuizCommand.endMessage());
+    }
 }
