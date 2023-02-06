c558b1287cd5a47f01ebfe6d851eb62ad53df080
Friederike von Gruben <friederike.von-gruben@informatik.hs-fulda.de>
2023-02-06 14:51:06 +0100

    unittest: Created BlackJackTest This test tests the Entity ID from the Player

---
+++ src/test/java/org/bitbiome/classes/BlackJackTest.java
@@ -0,0 +1,20 @@
+package org.bitbiome.classes;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BlackJackTest {
+
+    private static BlackJack bj;
+    @BeforeAll
+    public static void setUpTest() {
+        bj = new BlackJack("UnitTest");
+    }
+    @Test
+    public void testGetEntity() {
+        assertEquals(BlackJack.Entity.PLAYER, bj.getEntity(1));
+    }
+
+}
