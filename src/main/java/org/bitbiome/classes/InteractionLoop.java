1749b5f66351b5f14f30888347bcf06380487a47
David Hermann <redeagle.private@gmail.com>
2023-02-06 10:07:56 +0100

    refactoring: Outsourced System.out.println

--- src/main/java/org/bitbiome/classes/InteractionLoop.java
+++ src/main/java/org/bitbiome/classes/InteractionLoop.java
@@ -12,7 +12,7 @@ public class InteractionLoop {
     public void run(TravelEngine travelEngine) {
         boolean isRunning = true;
         if (travelEngine.getPlayer().getName().equals("null")) {
-            System.out.println(Colors.ANSI_BLUE + "Oh, ein Fremder!\nBist du bereit für dein womöglich größtes Abenteuer?\nDann sag mir doch zunächst wie du heißt: " + Colors.ANSI_RESET);
+            print(Colors.ANSI_BLUE + "Oh, ein Fremder!\nBist du bereit für dein womöglich größtes Abenteuer?\nDann sag mir doch zunächst wie du heißt: " + Colors.ANSI_RESET);
             String name = input.nextLine();
             JsonParser jp = new JsonParser();
             JSONObject playerconf = jp.getJSONObject("playerconfig.json");
@@ -20,13 +20,18 @@ public class InteractionLoop {
             travelEngine.getPlayer().setName(name);
             jp.writeObject("playerconfig.json", playerconf);
         }
-        System.out.println(Colors.ANSI_BG_CYAN + Colors.ANSI_BLACK + "Willkommen zu BitBiome " + travelEngine.getPlayer().getName() + "!" + Colors.ANSI_RESET + "\n\n");
+        print(Colors.ANSI_BG_CYAN + Colors.ANSI_BLACK + "Willkommen zu BitBiome " + travelEngine.getPlayer().getName() + "!" + Colors.ANSI_RESET + "\n\n");
         while (isRunning) {
             String line = input.nextLine().toLowerCase();
             if (!Boot.instance.getCmdListener().perform(line.toLowerCase().split(" ")[0], input, isRunning, line, travelEngine)) {
-                System.out.println(Colors.ANSI_RED + "Unbekannter Befehl - Siehe " + Colors.ANSI_PURPLE + "help\n" + Colors.ANSI_RESET);
+                print(Colors.ANSI_RED + "Unbekannter Befehl - Siehe " + Colors.ANSI_PURPLE + "help\n" + Colors.ANSI_RESET);
             }
         }
+    }
+
+    public boolean print(String message) {
+        System.out.println(message);
+        return true;
     }
 
 
