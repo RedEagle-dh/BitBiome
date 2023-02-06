b26a43dfcbb824f587895f2ea341542ae8e3eaa7
David Hermann <redeagle.private@gmail.com>
2023-02-06 11:24:45 +0100

    refactoring: Added comments to CommandListener

--- src/main/java/org/bitbiome/commands/CommandListener.java
+++ src/main/java/org/bitbiome/commands/CommandListener.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class CommandListener {
-
+    // This class is the API between the command interface and the user input
+    // This class is used to handle all the commands
+    // The commands are stored in a hashmap
     private HashMap<String, CommandAPI> commands;
 
     public CommandListener() {
