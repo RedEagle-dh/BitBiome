98e47536dbb0897356d9455eae767e61fa994736
David Hermann <redeagle.private@gmail.com>
2023-02-07 18:04:19 +0100

    refactoring: Edited unit test in non-act part
    Instance now static

--- src/test/java/org/bitbiome/commands/ShopCommandTest.java
+++ src/test/java/org/bitbiome/commands/ShopCommandTest.java
@@ -6,30 +6,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShopCommandTest {
-    final ShopCommand shopCommand = new ShopCommand();
     final Shop shop = new Shop(null, true, null, null);
     @Test
     public void testValidInput1(){
         boolean expected = true;
-        boolean result = shopCommand.validInput("1");
+        boolean result = ShopCommand.validInput("1");
         assertEquals(expected, result);
     }
     @Test
     public void testValidInput2(){
         boolean expected = true;
-        boolean result = shopCommand.validInput("2");
+        boolean result = ShopCommand.validInput("2");
         assertEquals(expected, result);
     }
     @Test
     public void testValidInput3(){
         boolean expected = true;
-        boolean result = shopCommand.validInput("3");
+        boolean result = ShopCommand.validInput("3");
         assertEquals(expected, result);
     }
     @Test
     public void testValidInput4(){
         boolean expected = true;
-        boolean result = shopCommand.validInput("4");
+        boolean result = ShopCommand.validInput("4");
         assertEquals(expected, result);
     }
 
