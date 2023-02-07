47cbcc474ef9d6ad2b6ab7a7edd8e1b17c678c39
David Hermann <redeagle.private@gmail.com>
2023-02-07 19:34:20 +0100

    unittest: Edited the test method of QuizCmd
    AssertEquals is now AssertTrue
    It's testing whether the text contains something now

--- src/test/java/org/bitbiome/commands/QuizCommandTest.java
+++ src/test/java/org/bitbiome/commands/QuizCommandTest.java
@@ -9,12 +9,12 @@ public class QuizCommandTest {
 
     @Test
     public void testStartMessage() {
-        assertEquals("Du hast das Quiz gestartet! Hinweis: Wähle deine Antwort, indem du die Zahl (1-4) eingibst. Ist deine Lösung richtig, erhälst du 5 Münzen. Viel Erfolg! \n", QuizCommand.starterMessage());
+        assertTrue(QuizCommand.starterMessage().contains("Du hast das Quiz gestartet!"));
     }
 
     @Test
     public void testEndMessage() {
-        assertEquals("Das Quiz ist vorbei!", QuizCommand.endMessage());
+        assertTrue(QuizCommand.endMessage().contains("Das Quiz ist vorbei!"));
     }
 
     @Test
